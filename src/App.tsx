15476cb3cb1144ff4b17288e7dc10d4445c60089
bibikov nikita <bibikovnikita777@gmail.com>
2024-06-25 14:26:34 +0600

    Dependencies installed and configured: react-router-dom, @mui/material, @emotion/react, @emotion/styled.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contacts from './pages/Contacts';
+import './App.css';
 
-const App = () => {
+const App: React.FC = () => {
     return (
-        <div>
-
-        </div>
+       <></>
     );
 };
 
