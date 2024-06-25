f7073e6264d34bdb200eb3a8b16efaace17a88bd
bibikov nikita <bibikovnikita777@gmail.com>
2024-06-25 15:39:08 +0600

    Made the component, Contact us

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -1,9 +1,11 @@
+import React from 'react';
 
-const Contacts = () => {
+const Contacts: React.FC = () => {
     return (
         <div>
-
-
+            <h1>Contacts Page</h1>
+            <p>Contact us through this page.</p>
+            <p>Whatsapp: <span>+966708840250</span></p>
         </div>
     );
 };
